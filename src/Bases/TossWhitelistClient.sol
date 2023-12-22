b1ba21c6c1b8006d0ffc2ea8eb3ce16ac069782c
Hernan Castro <magehernan@gmail.com>
2023-12-22 18:42:25 -0300

    Rename error NotInWhitelist to TossWhitelistNotInWhitelist

--- src/Bases/TossWhitelistClient.sol
+++ src/Bases/TossWhitelistClient.sol
@@ -12,18 +12,18 @@ abstract contract TossWhitelistClient {
     // keccak256(abi.encode(uint256(keccak256("tossplatform.storage.TossWhitelistClient")) - 1)) & ~bytes32(uint256(0xff))
     bytes32 private constant TossWhitelistClientStorageLocation = 0xe32c7d464d01bf4699c6877240dec52b29adf1b2f9e414a502cf561c22d22d00;
 
+    error TossWhitelistNotInWhitelist(address address_);
+
     function _getTossWhitelistClientStorage() private pure returns (TossWhitelistClientStorage storage $) {
         assembly {
             $.slot := TossWhitelistClientStorageLocation
         }
     }
 
-    error NotInWhitelist(address);
-
     modifier isInWhitelist(address user) {
         TossWhitelistClientStorage storage $ = _getTossWhitelistClientStorage();
         if (user != address(0) && $.whitelistAddress != address(0) && !ITossWhitelist($.whitelistAddress).isInWhitelist(user)) {
-            revert NotInWhitelist(user);
+            revert TossWhitelistNotInWhitelist(user);
         }
         _;
     }
